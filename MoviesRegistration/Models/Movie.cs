98d5d6fac15fd09ac2e0233b530d79da9b8df07c
Kendall <allmand.kendall@gmail.com>
2020-05-22 08:04:40 -0400

    first

--- MoviesRegistration/Models/Movie.cs
+++ MoviesRegistration/Models/Movie.cs
@@ -10,7 +10,7 @@ namespace MoviesRegistration.Models
             public string ID { get; set; }
             public string Title { get; set; }
             public string Genre { get; set; }
-             public int Year { get; set; }
+             public DateTime Year { get; set; }
              public string Actors { get; set; }
              public string Directors { get; set; }
     }
