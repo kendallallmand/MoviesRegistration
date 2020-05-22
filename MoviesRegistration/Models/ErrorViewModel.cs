8989159b792f614dc739499e700640ced216b8b4
Kendall <allmand.kendall@gmail.com>
2020-05-22 06:41:47 -0400

    first

---
+++ MoviesRegistration/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MoviesRegistration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MoviesRegistration/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesRegistration.Models
+{
+    public class Movie
+    {
+            public string ID { get; set; }
+            public string Title { get; set; }
+            public string Genre { get; set; }
+             public int Year { get; set; }
+             public string Actors { get; set; }
+             public string Directors { get; set; }
+    }
+
+    
+}
